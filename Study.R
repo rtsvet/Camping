1cdce64143d942eb6f0fca06ff1c8641474874f4
Rado Mitsu <rtzvetkov@yahoo.com>
2022-06-24 10:16:55 +0200

    plot pannels

--- Study.R
+++ Study.R
@@ -6,32 +6,79 @@ library(report)
 library(dplyr)
 library("ggplot2")
 library("GGally")
+library(multcomp)
 
+########################
+# the histograms
+########################
+par(mfrow=c(2,4))
+hist(dat$Total.Sleep.Duration)
+hist(dat$Deep.Sleep.Duration)
+hist(dat$REM.Sleep.Duration)
+hist(dat$Light.Sleep.Duration)
+
+hist(dat$Average.HRV)
+hist(dat$Average.Resting.Heart.Rate)
+hist(dat$Lowest.Resting.Heart.Rate)
+hist(dat$Temperature.Deviation)
+
+########################
+# the boxplots
+########################
+par(mfrow=c(2,4))
+
+boxplot(dat$Total.Sleep.Duration ~ dat$Camping, xlab = "Camping",
+        ylab = "Total.Sleep.Duration")
+boxplot(dat$Deep.Sleep.Duration ~ dat$Camping, xlab = "Camping",
+        ylab = "Deep Sleep Duratione")
+boxplot(dat$REM.Sleep.Duration ~ dat$Camping, xlab = "Camping",
+        ylab = "REM.Sleep.Duration")
+boxplot(dat$Light.Sleep.Duration ~ dat$Camping, xlab = "Camping",
+        ylab = "Light.Sleep.Duration")
+
+boxplot(dat$Average.HRV ~ dat$Camping, xlab = "Camping",
+        ylab = "Average.HRV")
+boxplot(dat$Average.Resting.Heart.Rate ~ dat$Camping, xlab = "Camping",
+        ylab = "Average.Resting.Heart.Rate")
+boxplot(dat$Lowest.Resting.Heart.Rate ~ dat$Camping, xlab = "Camping",
+        ylab = "Lowest Resting Heart Rate")
+boxplot(dat$Temperature.Deviation ~ dat$Camping, xlab = "Camping",
+        ylab = "Temperature.Deviation")
+
+
+#####################
 # HRV Model
-boxplot(dat$Average.HRV ~ dat$Camping)
+
 hrv_aov <- aov(Average.HRV ~ Camping, data = dat)
 summary(hrv_aov)
 report(hrv_aov)
 
 # Av RHR
-boxplot(dat$Average.Resting.Heart.Rate ~ dat$Camping)
 avrhr_aov <- aov(Average.Resting.Heart.Rate ~ Camping, data = dat)
 report(avrhr_aov)
 
 # lowest RHR
-boxplot(dat$Lowest.Resting.Heart.Rate ~ dat$Camping, xlab = "Camping",
-        ylab = "Lowest Resting Heart Rate")
 lorhr_aov <- aov(Lowest.Resting.Heart.Rate ~ Camping, data = dat)
 report(lorhr_aov)
 
 # lowest Deep Sleep
-boxplot(dat$Deep.Sleep.Duration ~ dat$Camping, xlab = "Camping",
-        ylab = "Deep Sleep Duratione")
 dsd_aov <- aov(Lowest.Resting.Heart.Rate ~ Camping, data = dat)
 report(dsd_aov)
 
-# lowest REM Sleep
-boxplot(dat$REM.Sleep.Duration ~ dat$Camping, xlab = "Camping",
-        ylab = "REM.Sleep.Duration")
+#  REM Sleep
 aov <- aov(Lowest.Resting.Heart.Rate ~ Camping, data = dat)
 report(aov)
+
+# Total Sleep duration
+aov <- aov(Total.Sleep.Duration ~ Camping, data = dat)
+report(aov)
+
+# Light Sleep duration
+aov <- aov(Light.Sleep.Duration ~ Camping, data = dat)
+report(aov)
+
+
+
+
+
+
